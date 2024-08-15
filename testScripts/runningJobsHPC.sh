5ff6875a783145edd8de19d32552af684d3d27e4
tim-external <timhansen93@googlemail.com>
2024-08-15 10:41:34 +0200

    added stuff

--- testScripts/runningJobsHPC.sh
+++ testScripts/runningJobsHPC.sh
@@ -2,7 +2,8 @@
 
 
 #python mainPredator.py predator/configs/train/indoor.yaml
+python trainDeepGlobaleRegistration.py --timnoise false --timnoisevar 0.01 --out_dir '/home/thansen/tensorboard/3dmatch/models/deepGlobalRegistration/1' --weights '/home/thansen/dataFolder/3dmatch/models/deepGlobalRegistration/dlModels/2019-08-16_19-21-47.pth' --threed_match_dir '/home/thansen/dataFolder/3dmatch/threedmatch/'
 
-python trainDeepGlobaleRegistration.py --timnoise false --timnoisevar 0.01 --out_dir '~/tensorboard/3dmatch/models/deepGlobalRegistration/1' --weights '~/dataFolder/3dmatch/models/deepGlobalRegistration/dlModels/2019-08-16_19-21-47.pth' --threed_match_dir '~/dataFolder/3dmatch/threedmatch'
+#python trainDeepGlobaleRegistration.py --timnoise false --timnoisevar 0.01 --out_dir '/home/thansen/tensorboard/3dmatch/models/deepGlobalRegistration/1' --weights '/home/thansen/dataFolder/3dmatch/models/deepGlobalRegistration/dlModels/2019-08-16_19-21-47.pth' --threed_match_dir '/home/thansen/dataFolder/3dmatch/threedmatch/'
 #sleep 2m
 #python trainDeepGlobaleRegistration.py --timnoise true --timnoisevar 0.05 --out_dir '~/tensorboard/3dmatch/models/deepGlobalRegistration/2' --weights '~/dataFolder/3dmatch/models/deepGlobalRegistration/dlModels/2019-08-16_19-21-47.pth' --threed_match_dir '~/dataFolder/3dmatch/threedmatch'
