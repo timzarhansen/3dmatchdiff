4ecdd8cec7de356998088f07f2556bc31c4abc24
tim-external <timhansen93@googlemail.com>
2024-10-03 12:21:14 +0200

    short update

--- testScripts/runningJobsDGR.sh
+++ testScripts/runningJobsDGR.sh
@@ -8,36 +8,45 @@ nn_max_n=250
 use_gpu=True
 val_max_iter=10
 
-additionalSettings=--max_epoch $numberofepochs --test_valid $validation --threed_match_dir $datasetdir --weights $weights --val_max_iter $val_max_iter --use_gpu $use_gpu
 
-
-echo $additionalSettings
-sleep $sleeptimebetweenruns
-sleep $sleeptimebetweenruns
 #python mainPredator.py predator/configs/train/indoor.yaml
 export OMP_NUM_THREADS=10;
 python trainDeepGlobaleRegistration.py --timnoise true --timnoiseval true --timnoisevar 0.05 --out_dir '/workspace/3dmatch/output/deepGlobalRegistration/10' \
-$additionalSettings
+--max_epoch $numberofepochs \
+--test_valid $validation \
+--threed_match_dir $datasetdir \
+--weights $weights \
+--val_max_iter $val_max_iter \
+--use_gpu $use_gpu
 
 
 sleep $sleeptimebetweenruns
 python trainDeepGlobaleRegistration.py --timnoise false --timnoiseval true --timnoisevar 0.05 --out_dir '/workspace/3dmatch/output/deepGlobalRegistration/11' \
-$additionalSettings
+--max_epoch $numberofepochs \
+--test_valid $validation \
+--threed_match_dir $datasetdir \
+--weights $weights \
+--val_max_iter $val_max_iter \
+--use_gpu $use_gpu
 
 sleep $sleeptimebetweenruns
 python trainDeepGlobaleRegistration.py --timnoise true --timnoiseval false --timnoisevar 0.05 --out_dir '/workspace/3dmatch/output/deepGlobalRegistration/12' \
-$additionalSettings
+--max_epoch $numberofepochs \
+--test_valid $validation \
+--threed_match_dir $datasetdir \
+--weights $weights \
+--val_max_iter $val_max_iter \
+--use_gpu $use_gpu
 
 
 sleep $sleeptimebetweenruns
 python trainDeepGlobaleRegistration.py --timnoise false --timnoiseval false --timnoisevar 0.05 --out_dir '/workspace/3dmatch/output/deepGlobalRegistration/13' \
-$additionalSettings
-#--max_epoch $numberofepochs \
-#--test_valid $validation \
-#--threed_match_dir $datasetdir \
-#--weights $weights \
-#--val_max_iter $val_max_iter \
-#--use_gpu $use_gpu
+--max_epoch $numberofepochs \
+--test_valid $validation \
+--threed_match_dir $datasetdir \
+--weights $weights \
+--val_max_iter $val_max_iter \
+--use_gpu $use_gpu
 
 
 
